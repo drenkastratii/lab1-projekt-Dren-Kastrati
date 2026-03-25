7310f0e3df9aee191295d57cc8a71aa714b3f736
drenko05 <drenkastrati05@hotmail.com>
2026-03-25 14:13:58 +0100

    Add database configuration using Sequelize and dotenv

---
+++ server/config/db.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({ path: path.join(__dirname, '../.env'), override: true });
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS || undefined,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        logging: false
+    }
+);
+
+module.exports = sequelize;
